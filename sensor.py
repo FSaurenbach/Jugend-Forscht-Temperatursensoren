cdc4e1df7e266de4aad67acaa711e8f4cbf9feb5
FSaurenbach <ferdinand161@outlook.de>
2022-12-14 08:48:43 +0100

    Changed grammar

--- sensor.py
+++ sensor.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import time  # Modul um das Programm für eine Bestimmte Zeit zu stoppen.
+import time  # Modul um das Programm für eine bestimmte Zeit zu stoppen.
 from datetime import datetime  # Modul um die aktuelle Zeit auszulesen.
 
 
@@ -7,7 +7,7 @@ def is_time():  # Funktion, die alle 5 Minuten die Daten ausliest.
     current_minute = datetime.now().strftime("%M")  # Die aktuelle Minute auslesen als Variable abspeichern.
     if "0" in current_minute or "5" in current_minute:  # Wenn eine 5 oder 0 in der aktuellen Minute ist, z.B 20 oder 25, Funktion richtig setzen.
         return True
-    else:  # Wenn keine 5 oder 0 in der aktuellen Minute ist die Funktion Falsch setzen
+    else:  # Wenn keine 5 oder 0 in der aktuellen Minute ist die Funktion falsch setzen
         return False
 
 
@@ -20,18 +20,18 @@ sensor1_text = open("Sensor1.txt",
                     "a")  # Datenbanken für die jeweiligen Temperaturwerte öffnen. "a" steht hier für "append" also Werte der Datei hinzufügen.
 sensor2_text = open("Sensor2.txt", "a")
 
-while True:  # Dauerschleife um alle 20 Sekunden zu überprüfen ob es Zeit ist Messungen zu machen(Funktion is_time())
+while True:  # Dauerschleife um alle 20 Sekunden zu überprüfen, ob es Zeit ist Messungen zu machen (Funktion is_time())
     if is_time():  # Wenn es Zeit ist: Temperatur auslesen und speichern
 
         data = open(sensor1, "r").read()  # Auf beide Sensoren erneut zugreifen um aktuelle Temperatur zu bekommen.
         data2 = open(sensor2, "r").read()
         (discard, sep, reading) = data.partition(' t=')  # Nur die Temperatur herausfiltern aus den Daten
         t = float(reading) / 1000.0  # Temperatur umrechenen
-        with open("Sensor1.txt", "a") as f:  # Datenbank öffnen und Temperatur spreichern.
+        with open("Sensor1.txt", "a") as f:  # Datenbank öffnen und Temperatur speichern.
             f.write("\n")
             now = datetime.now()
             current_minute = now.strftime("%H:%M")  # Die aktuelle Zeit zu den Temperaturdaten hinzufügen
-            f.write(current_minute + " ," + str(t))  # Abspeichern der daten
+            f.write(current_minute + " ," + str(t))  # Abspeichern der Daten
 
         # Gleiches Vorgehen
         (discard, sep, reading) = data2.partition(' t=')
